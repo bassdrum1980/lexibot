89e802ab9dfeaf1ea0ab321cf323a47f196dfc38
Alex Shevchenko <acidi@yandex.ru>
2022-07-12 16:30:32 -0700

    Added Input component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { useRef, useEffect } from 'react';
-import { Button } from 'components';
+import { useRef, useEffect, useState } from 'react';
+import { Button, Input } from 'components';
 
 const App = () => {
+  const [value, setValue] = useState('');
+
   const btnRef = useRef(null);
   useEffect(() => console.log(btnRef), []);
 
   return (
     <div>
       <Button className={['xl', 'active']} ref={btnRef}>Click me</Button>
+      <Input onChange={(e) => setValue(e.target.value)} placeholder="type smth" value={value} disabled />
+      <Input onChange={(e) => setValue(e.target.value)} placeholder="type smth" value={value} />
     </div>
   );
 };
---
+++ src/components/Input/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import './index.scss';
+
+const propTypes = {
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  name: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  isDisabled: PropTypes.bool,
+  isValid: PropTypes.bool,
+};
+
+const defaultProps = {
+  placeholder: '',
+  value: '',
+  name: '',
+  className: '',
+  isDisabled: false,
+  isValid: true,
+};
+
+const Input = ({
+  placeholder, value, name, onChange, className, isDisabled, isValid, ...rest
+}) => (
+  <input
+    type="text"
+    placeholder={placeholder}
+    value={value}
+    name={name}
+    onChange={onChange}
+    className={classnames(
+      'input',
+      Array.isArray(className) ? className.join(' ') : className,
+      { 'input--disabled': isDisabled },
+      { 'input--invalid': !isValid },
+    )}
+    disabled={isDisabled}
+    {...rest}
+  />
+);
+
+Input.displayName = 'Input';
+Input.propTypes = propTypes;
+Input.defaultProps = defaultProps;
+
+export default Input;
