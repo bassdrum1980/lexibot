c2b6a60d2bc60c067be0497874e396d648557eed
Alex Shevchenko <acidi@yandex.ru>
2022-07-13 11:39:19 -0700

    + Textarea component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { useRef, useEffect } from 'react';
-import { Button } from 'components';
+import { useRef, useEffect, useState } from 'react';
+import { Button, Textarea } from 'components';
 
 const App = () => {
+  const [value, setValue] = useState('');
+
   const btnRef = useRef(null);
   useEffect(() => console.log(btnRef), []);
 
   return (
     <div>
       <Button className={['xl', 'active']} ref={btnRef}>Click me</Button>
+      <Textarea name="textarea" onChange={(e) => setValue(e.target.value)} value={value} />
     </div>
   );
 };
