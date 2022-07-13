0ef516be46d46cd0b239e23b9310f10a47242d2d
Alex Shevchenko <acidi@yandex.ru>
2022-07-13 16:18:22 -0700

    + pill component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from 'react';
-import { Button } from 'components';
+import { Button, Pill } from 'components';
 
 const App = () => {
   const btnRef = useRef(null);
@@ -8,6 +8,26 @@ const App = () => {
   return (
     <div>
       <Button className={['xl', 'active']} ref={btnRef}>Click me</Button>
+      <hr />
+      <Pill type="noun">noun</Pill>
+      <br />
+      <Pill type="pronoun">pronoun</Pill>
+      <br />
+      <Pill type="interjection">interjection</Pill>
+      <br />
+      <Pill type="preposition">preposition</Pill>
+      <br />
+      <Pill type="conjunction">conjunction</Pill>
+      <br />
+      <Pill type="verb">verb</Pill>
+      <br />
+      <Pill type="adverb">adverb</Pill>
+      <br />
+      <Pill type="adjective">adjective</Pill>
+      <br />
+      <Pill type="various">photography</Pill>
+      ,
+      <Pill type="various">optics</Pill>
     </div>
   );
 };
---
+++ src/components/Pill/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import './index.scss';
+
+/**
+ * "type" — corresponds to the relating part of speech
+ * 'noun', 'interjection', 'preposition', 'conjunction',
+ * 'verb', 'adverb', 'adjective', 'various'
+ */
+
+const propTypes = {
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  type: PropTypes.string,
+};
+
+const defaultProps = {
+  className: '',
+  type: 'default',
+};
+
+const Pill = ({
+  children, type, className, ...rest
+}) => (
+  <div
+    className={classnames(
+      'pill',
+      Array.isArray(className) ? className.join(' ') : className,
+      `pill--${type}`,
+    )}
+    {...rest}
+  >
+    {children}
+  </div>
+);
+
+Pill.displayName = 'Pill';
+Pill.propTypes = propTypes;
+Pill.defaultProps = defaultProps;
+
+export default Pill;
