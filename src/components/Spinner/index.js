b0905cc4e0cec3cf4e46a139df31200f4983ea78
Alex Shevchenko <acidi@yandex.ru>
2022-07-16 14:52:12 -0700

    + spinner-component

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import './index.scss';
+
+const propTypes = {
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+};
+
+const defaultProps = {
+  className: '',
+};
+
+const Spinner = ({
+  className, ...rest
+}) => (
+  <div
+    className={classnames(
+      'spinner',
+      Array.isArray(className) ? className.join(' ') : className,
+    )}
+    {...rest}
+  />
+);
+
+Spinner.displayName = 'Spinner';
+Spinner.propTypes = propTypes;
+Spinner.defaultProps = defaultProps;
+
+export default Spinner;
