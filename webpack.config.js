ec7bc73af0f77da236ac8314e6276cfc234d4d68
Alex Shevchenko <acidi@yandex.ru>
2022-07-18 13:10:36 -0700

    + Merriweather font

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
         },
       },
       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
