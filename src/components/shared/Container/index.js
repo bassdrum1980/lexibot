94d3c66652e8f2104f8b5879d92b86333fe500ac
Alex Shevchenko <acidi@yandex.ru>
2022-08-21 14:39:38 -0700

    'Container' discerning routes and changing themes

--- src/components/shared/Container/index.js
+++ src/components/shared/Container/index.js
@@ -1,10 +1,24 @@
+import { useLocation } from 'react-router-dom';
+import classnames from 'classnames';
+import * as routes from '../../../routing';
 import './index.scss';
 
-const Container = ({ children }) => (
-  <div className="container">
-    {children}
-  </div>
-);
+const Container = ({ children }) => {
+  // Get current pathname, strip of leading '/',
+  // use it to define current theme.
+  let { pathname } = useLocation();
+  pathname = pathname.replace(/^\/+/g, '');
+
+  return (
+    <div className={classnames('container', {
+      'container--search': pathname === routes.searchURL,
+      'container--study': pathname === routes.studyURL,
+    })}
+    >
+      {children}
+    </div>
+  );
+};
 
 Container.displayName = 'Container';
 
