7a6ccf62c97f697393cc81c01761ad77dd265150
Alex Shevchenko <acidi@yandex.ru>
2022-08-25 10:55:48 -0700

    Create 'Warning' component

---
+++ src/components/shared/Warning/index.jsx
@@ -0,0 +1,70 @@
+/**
+ * Shows an array of system messages
+ * (errors, warnings, info)
+  */
+
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import { useLayoutEffect } from 'react';
+import { Button } from 'components';
+import './index.scss';
+
+const propTypes = {
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  title: PropTypes.string,
+  message: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+};
+
+const defaultProps = {
+  title: '',
+  message: '',
+  className: '',
+};
+
+const Warning = ({
+  className,
+  title,
+  message,
+  onClick,
+  ...rest
+}) => {
+  useLayoutEffect(() => {
+    // https://reactjs.org/docs/hooks-reference.html#useeffect
+    // мутировать дом нод через ref (container?) useref?
+    // возвращать функцию которая удаляет класс с контейнера
+  }, []);
+
+  return (
+    <div
+      className={classnames(
+        'warning',
+        Array.isArray(className) ? className.join(' ') : className,
+      )}
+      {...rest}
+    >
+      <div className="warning__content">
+        <div className="warning__content__wrapper">
+          {title && <h2>{title}</h2>}
+          {message && <p>{message}</p>}
+        </div>
+      </div>
+      <div className="warning__footer">
+        <Button
+          type="button"
+          btnStyle="secondary"
+          width="hug"
+          onClick={onClick}
+        >
+          Reset
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+Warning.displayName = 'Warning';
+Warning.propTypes = propTypes;
+Warning.defaultProps = defaultProps;
+
+export default Warning;
