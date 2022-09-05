86ff304fd83c58382a1fdb9e11efc75b104b2d94
Alex Shevchenko <acidi@yandex.ru>
2022-09-05 10:59:21 -0700

    Spinner style update

--- src/features/user/user-slice.js
+++ src/features/user/user-slice.js
@@ -13,7 +13,6 @@ export const fetchUser = createAsyncThunk('@@user/fetch-user', async (tgid, { ex
 
 const initialState = {
   user: null,
-  // loading: 'idle', // loading
 };
 
 export const userSlice = createSlice({
@@ -21,19 +20,11 @@ export const userSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: {
-    // [fetchUser.pending]: (state) => {
-    //   state.loading = 'loading';
-    // },
     [fetchUser.fulfilled]: (state, action) => {
-      // state.loading = 'idle';
       state.user = action.payload.data;
     },
-    // [fetchUser.rejected]: (state) => {
-    //   state.loading = 'idle';
-    // },
   },
 });
 
 export const userReducer = userSlice.reducer;
 export const selectUser = (state) => state.user;
-// export const selectLoading = (state) => state.user.loading;
