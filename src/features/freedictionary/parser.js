d01264690706c46ed952f0263a842108333e48ec
Alex Shevchenko <acidi@yandex.ru>
2022-09-20 14:59:31 -0700

    Update parser.js

--- src/features/freedictionary/parser.js
+++ src/features/freedictionary/parser.js
@@ -1,30 +1,32 @@
-/* eslint-disable no-restricted-syntax */
-import cloneDeep from 'lodash/cloneDeep';
-import { nanoid } from '@reduxjs/toolkit';
-
 /**
- * @param {Array} raw - raw data from the free dictionary api
+ * Parses the raw data from the free dictionary api,
+ * makes and returns an array of meanings
+ * ('meaning' — a set of definitions that share
+ * part of speech, phonetics and, sometimes, thesaurus).
+ * @param {Array} apiData
  * @returns {Array} of meanings
  */
 
-const parser = (raw) => {
-  // Clone incoming data
-  const apiData = cloneDeep(raw);
+import { nanoid } from '@reduxjs/toolkit';
 
-  // Resulting array of definitions grouped by meanings
+/* eslint-disable no-restricted-syntax */
+const parser = (apiData) => {
+  // resulting array of definitions grouped by meanings
   const meanings = [];
 
-  // API returns an array of lexical entries
-  for (const lexical of apiData) {
+  // apiData is an array of lexical entries
+  apiData.forEach((lexical) => {
     // lexical entry shares wordId and phonetics among multiple meanings
     const { word } = lexical;
-    // TODO: how to select a proper phonetic?
+
+    // TODO: how to pick a proper phonetic?
     const phoneticsText = '';
     const phoneticsAudio = '';
 
-    // meaning includes several definitions of same part of speech
-    for (const elem of lexical.meanings) {
-      const { partOfSpeech } = elem;
+    // meaning includes up to several definitions,
+    // shares part of speech and word id among them
+    lexical.meanings.forEach((m) => {
+      const { partOfSpeech } = m;
 
       const meaning = {};
       meaning.definitions = [];
@@ -32,23 +34,22 @@ const parser = (raw) => {
       meaning.id = nanoid();
 
       // definition is the smallest particicle of data
-      for (const def of elem.definitions) {
+      m.definitions.forEach((d) => {
         const definition = {};
         definition.word = word;
         definition.phoneticsText = phoneticsText;
         definition.phoneticsAudio = phoneticsAudio;
         definition.partOfSpeech = partOfSpeech;
-        definition.definition = def.definition;
-        definition.synonyms = [...def.synonyms];
-        definition.antonyms = [...def.antonyms];
-        definition.example = def.example;
+        definition.definition = d.definition;
+        definition.synonyms = [...d.synonyms];
+        definition.antonyms = [...d.antonyms];
+        definition.example = d.example;
         definition.id = nanoid();
         meaning.definitions.push(definition);
-      }
-
+      });
       meanings.push(meaning);
-    }
-  }
+    });
+  });
 
   return meanings;
 };
