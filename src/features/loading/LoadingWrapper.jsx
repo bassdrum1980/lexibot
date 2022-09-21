ecfb97cb90ae7241fd9a217ad37b57a1e4c0cc67
Alex Shevchenko <acidi@yandex.ru>
2022-09-21 12:11:15 -0700

    Update LoadingWrapper.jsx
    Improved readability.

--- src/features/loading/LoadingWrapper.jsx
+++ src/features/loading/LoadingWrapper.jsx
@@ -1,33 +1,34 @@
-import { Modal, Warning, Spinner } from 'components';
 import { useSelector } from 'react-redux';
+
+import { Modal, Warning, Spinner } from 'components';
 import { selectError, selectLoading } from 'features';
 
 const LoadingWrapper = ({ children }) => {
   const error = useSelector(selectError);
   const loading = useSelector(selectLoading);
 
-  return (
-    <>
-      {error
-        && (
-        <Modal>
-          <Warning
-            className="warning--error"
-            title={error}
-            message={'Looks like something unexpected\njust has happened...'}
-            onClick={() => console.log('click')}
-          />
-        </Modal>
-        )}
-      {loading === 'loading'
-        && (
-        <Modal>
-          <Spinner />
-        </Modal>
-        )}
-      { children }
-    </>
-  );
+  if (loading === 'loading') {
+    return (
+      <Modal>
+        <Spinner />
+      </Modal>
+    );
+  }
+
+  if (error) {
+    return (
+      <Modal>
+        <Warning
+          className="warning--error"
+          title={error}
+          message={'Looks like something unexpected\njust has happened...'}
+          onClick={() => console.log('click')}
+        />
+      </Modal>
+    );
+  }
+
+  return children;
 };
 
 LoadingWrapper.displayName = 'LoadingWrapper';
