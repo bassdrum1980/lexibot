0e073d75548f079acb18bb4316e19380d51f8875
Alex Shevchenko <acidi@yandex.ru>
2022-10-10 16:39:43 -0700

    Redirect to 'Search' by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import { ThemeProvider } from 'components';
 import { UserLoader, LoadingWrapper } from 'features';
+import { searchURL } from './routing';
 
-const App = () => (
-  <ThemeProvider>
-    <UserLoader>
-      <LoadingWrapper>
-        <Outlet />
-      </LoadingWrapper>
-    </UserLoader>
-  </ThemeProvider>
-);
+const App = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (pathname === '/') navigate(searchURL);
+  }, []);
+
+  return (
+    <ThemeProvider>
+      <UserLoader>
+        <LoadingWrapper>
+          <Outlet />
+        </LoadingWrapper>
+      </UserLoader>
+    </ThemeProvider>
+  );
+};
 
 export default App;
