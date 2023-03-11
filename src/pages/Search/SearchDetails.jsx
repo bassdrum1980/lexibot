e417b709168829d423800437d4928776e08e31b5
Alexander Shevchenko <acidi@icloud.com>
2023-03-10 18:17:02 -0800

    move 'success' to SearchDetails

--- src/pages/Search/SearchDetails.jsx
+++ src/pages/Search/SearchDetails.jsx
@@ -1,27 +1,39 @@
 import { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { Page, BackNav } from 'components';
+import { Page, BackNav, Success } from 'components';
 import { ConfigureCard } from 'features';
 import {
   resetCurrentDefinitionId,
   postCard,
+  selectCardPosted,
 } from '../../features/freedictionary/freedictionary-slice';
 
 const SearchDetails = () => {
-  // handle back to search
   const dispatch = useDispatch();
+
+  // handle back to search
   const handleBack = useCallback(() => {
     dispatch(resetCurrentDefinitionId());
   }, []);
+
+  // handle card submit
   const handleSubmit = useCallback((card) => {
     dispatch(postCard(card));
   }, []);
 
+  // if card has just been posted -- show successs
+  const isCardPosted = useSelector(selectCardPosted);
+
   return (
     <Page>
-      <BackNav title="Back to search" onClick={handleBack} />
-      <ConfigureCard handleSubmit={handleSubmit} />
+      {isCardPosted && <Success>Card has been saved!</Success>}
+      {!isCardPosted && (
+        <>
+          <BackNav title="Back to search" onClick={handleBack} />
+          <ConfigureCard handleSubmit={handleSubmit} />
+        </>
+      )}
     </Page>
   );
 };
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,27 +1,13 @@
 import { useSelector } from 'react-redux';
 
-import { Success } from 'components';
 import SearchDetails from './SearchDetails';
 import SearchIndex from './SearchIndex';
-import {
-  selectDictonaryCurrentId,
-  selectCardPosted,
-} from '../../features/freedictionary/freedictionary-slice';
+import { selectDictonaryCurrentId } from '../../features/freedictionary/freedictionary-slice';
 
 const Search = () => {
   const currentDefinitionId = useSelector(selectDictonaryCurrentId);
-  const isCardPosted = useSelector(selectCardPosted);
 
-  // if card has just been posted — show success
-  if (isCardPosted) {
-    return <Success>Card has been saved!</Success>;
-  }
-
-  // if there is an active definition, show details
-  if (currentDefinitionId) return <SearchDetails />;
-
-  // otherwise, show the search itself
-  return <SearchIndex />;
+  return currentDefinitionId ? <SearchDetails /> : <SearchIndex />;
 };
 
 Search.displayName = 'Search Root Page';
