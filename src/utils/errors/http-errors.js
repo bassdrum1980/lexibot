48978a8e6633b959df533b440148cf2bad02e232
Alexander Shevchenko <acidi@icloud.com>
2023-04-04 22:32:46 -0700

    add 'type' to the errors

--- src/utils/errors/http-errors.js
+++ src/utils/errors/http-errors.js
@@ -1,8 +1,8 @@
 import { BaseError } from './index.js';
 
 export class HttpError extends BaseError {
-  constructor(title, message, statusCode) {
-    super(title, message);
+  constructor(type, title, message, statusCode) {
+    super(type, title, message);
     this.statusCode = statusCode;
   }
 }
--- src/utils/errors/index.js
+++ src/utils/errors/index.js
@@ -1,7 +1,11 @@
+// type - error, warning, info
+// used by the 'Warning' component to determine the style of the warning
+
 export class BaseError extends Error {
-  constructor(title, message) {
+  constructor(type, title, message) {
     super(message);
     this.name = this.constructor.name;
     this.title = title;
+    this.type = type;
   }
 }
