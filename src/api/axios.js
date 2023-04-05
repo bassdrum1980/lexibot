8d8b81bc75269f4956389b98f66d514a1bedbb8f
Alexander Shevchenko <acidi@icloud.com>
2023-04-04 22:33:42 -0700

    Update axios.js
    remove interceptors

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,27 +1,6 @@
 /* eslint-disable no-console */
 import axios from 'axios';
 
-// Errors logging
-axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response) {
-      console.log('Problem with the response');
-      console.log(`Response status - ${error.response.status}`);
-      console.log(`Response data - ${JSON.stringify(error.response.data)}`);
-    } else if (error.request) {
-      console.log('Problem with the request');
-      console.log(`error - ${error.request}`);
-    } else {
-      console.log(
-        'Something happened in setting up the request that triggered an Error'
-      );
-      console.log(`error - ${error.message}`);
-    }
-    return Promise.reject(error);
-  }
-);
-
 export const userInstance = axios.create({
   baseURL: 'http://localhost:3001/users',
   headers: {
