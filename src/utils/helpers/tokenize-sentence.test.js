f183c872ef33548a245aee1b68b2375301fab1f7
Alexander Shevchenko <acidi@icloud.com>
2023-04-06 17:17:07 -0700

    switch tokenizeSentence to ts
    updated the helper
    updated related test file (removed tests that are covered by typescript)

--- src/utils/helpers/tokenize-sentence.test.js
+++ src/utils/helpers/tokenize-sentence.test.js
@@ -4,20 +4,8 @@ describe('sunny day scenarios', () => {
   const sentence = 'Firmly or securely fixed in place; stable.';
   const result = tokenizeSentence(sentence);
 
-  test('the returned value is an array', () => {
-    expect(result).toBeInstanceOf(Array);
-  });
-
   test('the resulting array has 7 tokens', () => {
     expect(result.length).toEqual(7);
-  });
-
-  test('every token has value, id and isActive properties', () => {
-    result.forEach((token) => {
-      expect(token).toHaveProperty('value');
-      expect(token).toHaveProperty('id');
-      expect(token).toHaveProperty('isActive');
-    });
   });
 
   test('every token has isActive: true by default', () => {
@@ -31,16 +19,21 @@ describe('edge cases', () => {
   const emptySentence = '';
   const emptyResult = tokenizeSentence(emptySentence);
 
-  const wrongDataTypeSentence = null;
-  const tokenizeWrongDataType = () => {
-    tokenizeSentence(wrongDataTypeSentence);
-  };
-
   test('given empty string returns empty array', () => {
     expect(emptyResult).toEqual([]);
   });
 
-  test('given wrong dataType throws an error', () => {
-    expect(tokenizeWrongDataType).toThrow();
+  const sentenceWithOnlySpaces = '   ';
+  const resultWithOnlySpaces = tokenizeSentence(sentenceWithOnlySpaces);
+
+  test('given string with only spaces returns empty array', () => {
+    expect(resultWithOnlySpaces).toEqual([]);
+  });
+
+  const sentenceWithDoubleSpacings = '  test  test  ';
+  const resultWithDoubleSpacings = tokenizeSentence(sentenceWithDoubleSpacings);
+
+  test('given string with double spacings returns array with 2 tokens', () => {
+    expect(resultWithDoubleSpacings.length).toEqual(2);
   });
 });
