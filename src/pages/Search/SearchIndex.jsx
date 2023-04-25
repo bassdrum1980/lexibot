907a651718f65cddd982e3cafae0dd6771c5ecd0
Alexander Shevchenko <acidi@icloud.com>
2023-04-25 13:05:11 -0700

    add sign-out button
    temporary solution, to be removed

--- src/pages/Search/SearchIndex.jsx
+++ src/pages/Search/SearchIndex.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import classnames from 'classnames';
 
 import SearchField from 'features/query/SearchField';
@@ -6,8 +6,11 @@ import SearchResults from 'features/freedictionary/SearchResults';
 import { selectDictionaryMeanings } from 'features/freedictionary/freedictionary-slice';
 import { Page } from 'components';
 
+import { signOut } from 'features/user/user-slice';
+
 const SearchIndex = () => {
   const results = useSelector(selectDictionaryMeanings);
+  const dispatch = useDispatch();
 
   return (
     <Page
@@ -17,6 +20,13 @@ const SearchIndex = () => {
     >
       <SearchField />
       {Boolean(results.length) && <SearchResults />}
+      <button
+        onClick={() => {
+          dispatch(signOut());
+        }}
+      >
+        Sign out
+      </button>
     </Page>
   );
 };
