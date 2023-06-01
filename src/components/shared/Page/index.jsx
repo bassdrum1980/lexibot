41de1e3763bfda4940a6e5adb644d6f8527364a9
Alexander Shevchenko <acidi@icloud.com>
2023-05-31 17:21:40 -0700

    update page - add todo items

--- src/components/shared/Page/index.jsx
+++ src/components/shared/Page/index.jsx
@@ -1,8 +1,11 @@
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
-
 import './index.scss';
 
+// TODO: add page__content-wrapper div (wrap it around page__content)
+// page__content will remain scrollable
+// page__content will be clipped (to hide under header, but showing it's scrollbar)
+// see index.scss for details
 const propTypes = {
   className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
 };
@@ -11,16 +14,14 @@ const defaultProps = {
   className: '',
 };
 
-const Page = ({
-  className,
-  children,
-}) => (
-  <div className={classnames(
-    'page',
-    Array.isArray(className) ? className.join(' ') : className,
-  )}
+const Page = ({ className, children }) => (
+  <div
+    className={classnames(
+      'page',
+      Array.isArray(className) ? className.join(' ') : className
+    )}
   >
-    {children}
+    <div className="page__content">{children}</div>
   </div>
 );
 
