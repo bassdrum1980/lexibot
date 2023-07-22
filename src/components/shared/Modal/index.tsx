72412a103cb24400791eda0222909983eb5396c0
Alexander Shevchenko <acidi@icloud.com>
2023-07-21 23:26:31 -0700

    add classnames support

--- src/components/shared/Modal/index.tsx
+++ src/components/shared/Modal/index.tsx
@@ -1,17 +1,29 @@
 import { useEffect } from 'react';
+import classnames from 'classnames';
+
 import './index.scss';
 
 type ModalProps = {
   children: React.ReactNode;
+  className?: string | string[];
 };
 
-const Modal = ({ children }: ModalProps) => {
+const Modal = ({ children, className }: ModalProps) => {
   useEffect(() => {
     document.body.classList.add('modal-open');
     return () => document.body.classList.remove('modal-open');
   }, []);
 
-  return <div className="modal">{children}</div>;
+  return (
+    <div
+      className={classnames(
+        'modal',
+        Array.isArray(className) ? className.join(' ') : className
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 Modal.displayName = 'Modal';
