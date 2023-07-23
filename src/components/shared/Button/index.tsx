30b2369a8ec28cf414cec822c2ec0d589e613559
Alexander Shevchenko <acidi@icloud.com>
2023-07-22 18:00:24 -0700

    typescript migration

---
+++ src/components/shared/Button/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import classnames from 'classnames';
+import './index.scss';
+
+type ButtonType = {
+  type?: 'button' | 'reset' | 'submit';
+  size?: 's' | 'sm' | 'm' | 'l' | 'inline';
+  width?: 'fill' | 'hug';
+  btnStyle?:
+    | 'primary'
+    | 'secondary'
+    | 'tertiary'
+    | 'plain'
+    | 'destructive'
+    | 'legend'
+    | 'link'
+    | 'pseudo'
+    | 'icon';
+  className?: string | string[];
+  onClick?: () => void;
+  children: React.ReactNode;
+};
+
+const Button = ({
+  type = 'button',
+  size = 'm',
+  width = 'fill',
+  btnStyle = 'primary',
+  className,
+  onClick,
+  children,
+  ...rest
+}: ButtonType) => (
+  <button
+    type={type}
+    onClick={onClick}
+    className={classnames(
+      'button',
+      Array.isArray(className) ? className.join(' ') : className,
+      `button--${btnStyle}`,
+      `button--${size}`,
+      `button--${width}`
+    )}
+    {...rest}
+  >
+    {children}
+  </button>
+);
+Button.displayName = 'Button';
+
+export default Button;
