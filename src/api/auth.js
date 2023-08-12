df910fec06a06b2d20ab537f520f68eb7bf35ac4
Alexander Shevchenko <acidi@icloud.com>
2023-08-12 13:32:36 -0700

    removed redundant api method

--- src/api/auth.js
+++ src/api/auth.js
@@ -135,33 +135,3 @@ export const postSignIn = async ({ email, password }) => {
     );
   }
 };
-
-/**
- * Get User Profile
- */
-export const getUserProfile = async ({ token }) => {
-  try {
-    const response = await authInstance.get('/user', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    if (error.response && error.response.status === 401) {
-      throw new HttpError(
-        'error',
-        'Oops! Unauthorized',
-        'You are not authorized to access this resource. Please sign in.',
-        401
-      );
-    }
-
-    throw new HttpError(
-      'error',
-      'Something Went Wrong',
-      "We're sorry, but there was an issue with the server. Please try again later.",
-      error?.response?.status
-    );
-  }
-};
