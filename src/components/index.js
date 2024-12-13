8abd6dafe00e104e1ae6ccfd0b4c9ded1bc4d952
Sasha Shevchenko <acidi@icloud.com>
2024-12-12 20:19:19 -0800

    rearranges coponents' reexports

--- src/components/index.js
+++ src/components/index.js
@@ -1,34 +1,42 @@
-export { default as Banner } from './study/Banner';
-export { default as BannerCompleted } from './study/BannerCompleted';
-export { default as BannerNothing } from './study/BannerNothing';
-export { default as AppNav } from './shared/AppNav';
-export { default as PageHeader } from './shared/PageHeader';
-export { default as PageFooter } from './shared/PageFooter';
+// Shared Components
 export { default as Alert } from './shared/Alert';
-export { default as CropExample } from './search/CropExample';
+export { default as AppNav } from './shared/AppNav';
+export { default as BackNav } from './shared/BackNav';
 export { default as Backdrop } from './shared/Backdrop';
-export { default as Success } from './shared/Success';
-export { default as AddVariant } from './search/AddVariant';
-export { default as ExamplePicker } from './search/ExamplePicker';
-export { default as VariantPicker } from './search/VariantPicker';
-export { default as CardFormBody } from './search/CardFormBody';
-export { default as CardFormSection } from './search/CardFormSection';
-export { default as CardFormHeader } from './search/CardFormHeader';
-export { default as Cards } from './search/Cards';
-export { default as Card } from './search/Card';
-export { default as Controls } from './shared/Controls';
-export { default as Page } from './shared/Page';
-export { default as Modal } from './shared/Modal';
-export { default as Warning } from './shared/Warning';
-export { default as ThemeProvider } from './shared/ThemeProvider';
 export { default as Button } from './shared/Button';
 export { default as ButtonLegend } from './shared/ButtonLegend';
-export { default as Spinner } from './shared/Spinner';
-export { default as Week } from './study/Week';
-export { default as Pill } from './shared/Pill';
-export { default as Progress } from './study/Progress';
-export { default as BackNav } from './shared/BackNav';
+export { default as Controls } from './shared/Controls';
 export { default as FormGroup } from './shared/FormGroup';
 export { default as Input } from './shared/Input';
 export { default as InputWithAction } from './shared/InputWithAction';
+export { default as Modal } from './shared/Modal';
+export { default as Page } from './shared/Page';
+export { default as PageFooter } from './shared/PageFooter';
+export { default as PageHeader } from './shared/PageHeader';
+export { default as Pill } from './shared/Pill';
+export { default as Spinner } from './shared/Spinner';
+export { default as Success } from './shared/Success';
 export { default as Textarea } from './shared/Textarea';
+export { default as ThemeProvider } from './shared/ThemeProvider';
+export { default as Warning } from './shared/Warning';
+
+// Search Components
+export { default as AddVariant } from './search/AddVariant';
+export { default as Card } from './search/Card';
+export { default as CardFormBody } from './search/CardFormBody';
+export { default as CardFormHeader } from './search/CardFormHeader';
+export { default as CardFormSection } from './search/CardFormSection';
+export { default as Cards } from './search/Cards';
+export { default as CropExample } from './search/CropExample';
+export { default as ExamplePicker } from './search/ExamplePicker';
+export { default as VariantPicker } from './search/VariantPicker';
+
+// Study Components
+export { default as Banner } from './study/Banner';
+export { default as BannerCompleted } from './study/BannerCompleted';
+export { default as BannerNothing } from './study/BannerNothing';
+export { default as Progress } from './study/Progress';
+export { default as Week } from './study/Week';
+
+// Storybook Components
+export { default as StorybookBgContainer } from './storybook/BackgroundContainer';
