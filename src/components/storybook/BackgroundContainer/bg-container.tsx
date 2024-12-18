9d892437fd0c9d809ba33acc9a945bdf681b1c9a
Sasha Shevchenko <acidi@icloud.com>
2024-12-17 17:53:44 -0800

    adds themes support to storybook container

--- src/components/storybook/BackgroundContainer/bg-container.tsx
+++ src/components/storybook/BackgroundContainer/bg-container.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import './index.scss';
+import './bg-container.scss';
 
-const backgroundClasses = ['study', 'study-gray', 'search', 'search-gray'];
+const backgroundClasses = ['default', 'default-gray', 'search', 'search-gray'];
 
 export const StorybookBgContainer = ({
   children,
 }: {
   children: React.ReactNode;
-}) => (
-  <div>
-    {backgroundClasses.map((name) => (
-      <div
-        className={`storybook-bg-container storybook-bg-container--${name}`}
-        key={name}
-      >
-        {children}
-      </div>
-    ))}
-  </div>
-);
+}) => {
+  const getClassNames = (name: string): string => {
+    const theme = `theme-${name.split('-')[0]}`;
+    const container = `storybook-bg-container storybook-bg-container--${name}`;
+
+    return `${container} ${theme}`;
+  };
+
+  return (
+    <div>
+      {backgroundClasses.map((name) => (
+        <div
+          className={getClassNames(name)}
+          key={name}
+        >
+          {children}
+        </div>
+      ))}
+    </div>
+  );
+};
